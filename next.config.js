0c72b99b28cdaf4851d18ca5c4d4ed1db0082b68
JOAO SERAFIN <admin@akvo-esg.com.br>
2023-06-02 16:46:16 -0300

    feat: teste next 12

--- next.config.js
+++ next.config.js
@@ -3,14 +3,41 @@
 
 // module.exports = nextConfig
 
-const withPWA = require('next-pwa')
+// const withPWA = require('next-pwa')
 
-module.exports = withPWA({
-    // reactStrictMode: true,
-    pwa: {
-        dest: 'public',
-        registrer: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === 'development'
-    }
-})
+// module.exports = withPWA({
+//     // reactStrictMode: true,
+//     pwa: {
+//         dest: 'public',
+//         registrer: true,
+//         skipWaiting: true,
+//         disable: process.env.NODE_ENV === 'development'
+//     }
+// })
+
+
+
+
+
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+
+        return config;
+    },
+    swcMinify: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development",
+    },
+};
+
+const withPWA = require("next-pwa")({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+});
+
+module.exports = withPWA(nextConfig);
+
+
