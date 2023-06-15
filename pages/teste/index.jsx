eea545245b406a4187736c41ae2a67fd485aba04
joaoserafin <joaomserafin@gmail.com>
2023-06-15 01:15:54 -0300

    feat: shopClassics cards

--- pages/teste/index.jsx
+++ pages/teste/index.jsx
@@ -1,5 +1,16 @@
 
+// import { faLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { AiOutlineLeft } from '@react-icons/all-files/ai/AiOutlineLeft'
+import Link from "next/link";
+import React, { useRef, useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+// import styles from './index.module.css'
+
+import { EffectCards } from "swiper";
 
 export default function Teste () {
 
@@ -7,8 +18,42 @@ export default function Teste () {
 
 
     return (
-        <div>
-            testestestes
+        <div className='fadeItem'>
+            <div className="row d-flex py-3 ps-3">
+                <div className="col-2">
+                    <Link href='/shopping'>
+                        <AiOutlineLeft />
+                    </Link>
+                </div>
+                <div className="col-8 d-flex justify-content-center">
+
+                    <span>Clássicas</span>
+                </div>
+            </div>
+            <div className="row">
+
+
+
+                <Swiper
+                    effect={"cards"}
+                    grabCursor={true}
+                    modules={[EffectCards]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide className="d-flex justify-content-center align-items-start pe-5">
+                        <img src="/AZUL_LISTRA.png" alt="" height={400} />
+                    </SwiperSlide>
+                    <SwiperSlide className="d-flex justify-content-center align-items-start pe-5">
+                        <img src="/AMARELO_LISTRA.png" alt="" height={400} />
+                    </SwiperSlide>
+                    <SwiperSlide className="d-flex justify-content-center align-items-start pe-5">
+                        <img src="/BRANCA_LISTRA.png" alt="" height={400} />
+                    </SwiperSlide>
+                </Swiper>
+
+
+
+            </div>
         </div>
     )
 }
